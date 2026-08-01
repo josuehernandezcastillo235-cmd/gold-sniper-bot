09242a1855217620654338e937abe007922fed13
josuehernandezcastillo235-cmd <josuehernandezcastillo235@gmail.com>
2026-08-01 01:57:10 -0600

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -4,10 +4,13 @@ from ta.trend import EMAIndicator
 from ta.momentum import RSIIndicator
 
 def analizar():
-    df = yf.download("GC=F", period="5d", interval="5m")
+    df = yf.download("GC=F", period="5d", interval="5m", auto_adjust=False)
 
     if df.empty:
         return "❌ No se pudieron obtener datos."
+
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
 
     df["EMA20"] = EMAIndicator(df["Close"], window=20).ema_indicator()
     df["EMA50"] = EMAIndicator(df["Close"], window=50).ema_indicator()
