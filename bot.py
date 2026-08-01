4a425c849f197c75ced36a8d099c1d2c5e316d9f
josuehernandezcastillo235-cmd <josuehernandezcastillo235@gmail.com>
2026-08-01 02:56:02 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,11 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 async def main():
     bot = Bot(token=TOKEN)
+    
+    await bot.send_message(
+    chat_id=CHAT_ID,
+    text="🚀 Gold Sniper Bot iniciado correctamente en Railway."
+    )
 
     while True:
         señal = analizar()
