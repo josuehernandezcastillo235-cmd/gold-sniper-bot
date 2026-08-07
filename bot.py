e110b1e3cba7854b4b3fd92664396c7c4f236dd9
josuehernandezcastillo235-cmd <josuehernandezcastillo235@gmail.com>
2026-08-07 12:37:31 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,6 +146,9 @@ async def botones(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def analizar_loop(app):
 
+
+async def analizar_loop(app):
+
     global ultima_senal, ultima_alerta_tiempo
 
     while True:
